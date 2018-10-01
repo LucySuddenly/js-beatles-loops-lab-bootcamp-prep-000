5d8595596710c50c3c9e69a23c27d81c9126f331
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-01 23:00:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts(facts) {
   }
   function iLoveTheBeatles(number) {
     var = array[]
-    do { array.push
+    do { array.push("I love the Beatles!")
       
     } while
   }
