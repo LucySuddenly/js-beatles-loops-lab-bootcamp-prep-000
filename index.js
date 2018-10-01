ae75fe26b18f50cecdf681dddfd5452be31900ee
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-01 22:48:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var Beatle = []
+  for (var i = 0; i < musician.length; i++){
+    Beatle[i] = `${musician[i]} plays ${instrument[i]}` 
+  } return Beatle
+} 
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {facts[i] = facts[i] + '!!!'
+  } return facts
+  i++
+}
